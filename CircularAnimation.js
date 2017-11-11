06d166d38739e2fcf88245ed575f1127c942539a
margaridaviterbo <margaridaviterbo@hotmail.com>
2017-11-11 08:52:18 +0000

    circular animation done

--- CircularAnimation.js
+++ CircularAnimation.js
@@ -5,25 +5,21 @@
 class CircularAnimation extends Animation{
 	constructor(scene, center, radius, initAngle, rotAngle, velocity) {
 		super();
+
+		var toRad = function(deg) {
+			return deg*(Math.PI/180.0);
+		}
 		
 		this.scene = scene;
 		this.center = center;
 		this.radius = radius;
-		this.initAngle = initAngle;
-		this.rotAngle = rotAngle;
+		this.initAngle = toRad(initAngle);
+		this.rotAngle = toRad(rotAngle);
+		this.angle = this.initAngle;
+		this.endAngle = this.initAngle + this.rotAngle;
 		this.velocity = velocity;
 
-
-		this.positionX = 0;
-        this.positionY = 0;
-        this.positionZ = 0;
-
-        this.controlVar = -1;
         this.previousCurrTime = 0;
-
-        this.angle = 0;
-        this.direction = [0, 0, 0];
-        this.previousDirection = [0, 0, 0];
 
 	}
 
@@ -39,29 +35,15 @@ class CircularAnimation extends Animation{
         var dt = (currTime - this.previousCurrTime)/1000;
         this.previousCurrTime = currTime;
        
-        if(this.angle < this.rotAngle){
-
-			//TODO adaptar o movimento para ter em conta o centro da circunferencia e o angulo inicial
-
-		   
-			
-			var distance = this.radius * this.rotAngle;
-
-			this.angle += distance * dt * this.velocity;
-
-            this.positionX = this.center[0] + Math.cos(this.angle - this.initAngle) * this.radius;
-           // var y = this.direction[1];
-		   this.positionZ = this.center[2] + Math.sin(this.angle - this.initAngle) * this.radius;
-
-			//this.positionX += x*dt*this.velocity;
-			//this.positionZ += z*dt*this.velocity;
-			//this.positionY += y*dt*this.velocity;
-             
+        if(this.angle < this.endAngle){
+			this.angle += dt * this.velocity;
         }
     }
 
     push(){
-        this.scene.translate(this.positionX, 0, this.positionZ);
+		this.scene.translate(this.center[0], this.center[1], this.center[2]);
+		this.scene.rotate(this.angle, 0, 1, 0);
+		this.scene.translate(this.radius, 0, 0);
     }
 	
 }
--- MyGraphNode.js
+++ MyGraphNode.js
@@ -25,7 +25,7 @@ function MyGraphNode(graph, nodeID) {
 
     //TODO eventualmente dar parse das animaçoes do xml e escolher qual animaçao criar ou quais
     if(this.nodeID == "jornal"){
-        this.animation = new CircularAnimation(this.graph.scene, [0, 0, 0], 5, 0, 45, 10);        
+        this.animation = new CircularAnimation(this.graph.scene, [5, 5, 5], 10, 90, 180, 0.5);        
     }
 }
 
