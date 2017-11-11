4f484b73c91122e28acfeedc797725e35abcea35
margaridaviterbo <margaridaviterbo@hotmail.com>
2017-11-11 17:00:28 +0000

    more on  bezier animation

--- MyGraphNode.js
+++ MyGraphNode.js
@@ -25,7 +25,7 @@ function MyGraphNode(graph, nodeID) {
 
     //TODO eventualmente dar parse das animaçoes do xml e escolher qual animaçao criar ou quais
     if(this.nodeID == "jornal"){
-        this.animation = new BezierAnimation(this.graph.scene, [[1, 1, 1], [3, 3, 3], [5, 5, 5], [7, 7, 7]], 1);        
+        this.animation = new BezierAnimation(this.graph.scene, [[0, 0, 0], [1, 0, 0], [1, 1, 0], [0, 1, 0]], 1);        
     }
 }
 
