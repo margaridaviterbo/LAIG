0956e54cec1269895a2abb0d1f8e25cdf11dbf64
Ana Rita Torres <anaritatorres96@outlook.pt>
2017-12-18 13:41:19 +0000

    almost there with the prolog

--- Tp3/reader/Prolog.js
+++ Tp3/reader/Prolog.js
@@ -9,12 +9,106 @@ function Prolog(game){
 }
 Prolog.prototype.constructor = Prolog;
 
+
+
+//in case of ad request
 Prolog.prototype.handleReply = function(data){
     if (data.target.response == "Bad Request") {
         return;
     }
    
 }
+
+//types of replies
+success = function(reply){
+
+    var splitReply = reply.split(',');
+    if (splitReply[3] == "true"){
+        return true;
+    }
+     
+    return false;
+}
+
+gameOver = function(reply){
+
+    var splitReply = reply.split(',');
+    if (splitReply[2] == "true"){
+         return true;
+    }
+    return false;
+}
+
+play = function(reply){
+    var splitReply = reply.split(',');
+    var gameOver;
+
+    if (splitReply[2] == "true"){
+        gameOver = true;
+    } 
+    else gameOver = false;
+
+    return new Play(splitReply[4],
+                    parseInt(splitReply[5]),
+                    parseInt(splitReply[6]),
+                    parseInt(splitReply[7]),
+parseInt(splitReply[8]),gameOver);
+}
+
+//make a request
+Prolog.prototype.getPrologRequest = function(requestObject, success, error, port){
+    
+    var requestString;
+    var requestPort = port || 8081
+    var request = new XMLHttpRequest();
+    var input = this;
+
+    if(requestObject == "quit"){
+        requestString = "quit" ;
+    } 
+    else if (requestObject.botPlay){
+       
+        if(requestObject.botDifficulty == 0){
+            requestString = requestObject.randomPlay();
+        }
+        else{
+            requestString = requestObject.smartPlay();
+        }
+    }
+    else{
+        requestString = requestObject.humanPlay();
+    }
+
+    request.open('GET', 'http://localhost:' + requestPort + '/' + requestString, true);
+
+    request.onload = onSuccess || function (data) {
+
+        var response = data.target.response;
+
+        if(requestObject.botPlay){
+
+            var play = play(response);
+            // Animate piece and make play
+        }
+        else{
+            if (success(response) == true) {
+                var gameOver = gameOver(response);
+                // Animate piece and make play
+            }
+        }
+
+    };
+
+    request.onerror = error || function () {
+        console.log("Error waiting for response");
+    };
+
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    request.send();
+
+    return request;
+}
+    
 
 //request constructor
 function Request(player, x, y, targetX, targetY, ivoryIn, cigarIn, board, botPlay,botDifficulty){
