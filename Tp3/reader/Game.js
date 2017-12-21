b5407a16a924664ce05bff3ba22b07fc3dd10141
margaridaviterbo <margaridaviterbo@hotmail.com>
2017-12-21 13:04:04 +0000

    recognizing valid play

--- Tp3/reader/Game.js
+++ Tp3/reader/Game.js
@@ -9,6 +9,7 @@ function Game(scene){
     this.board = this.scene.graph.board;
     this.bot = false;//implementar depois
     this.difficulty = null;//implementar
+    this.reply = [];
     
 }
 
@@ -17,37 +18,41 @@ Game.prototype.constructor = Game;
 
 Game.prototype.update = function(){
 
+    //ivorySize,cigarSize,endGame,Success
+
     switch(this.state){
         case 0:
-            console.log(this.board.getSelectedTile(this.board.selectedTileID[0]));
+            //console.log(this.board.getSelectedTile(this.board.selectedTileID[0]));
             if(this.board.selectedTileID[0] != null && this.board.getSelectedTile(this.board.selectedTileID[0]).piece != null){
                 if(this.board.getSelectedTile(this.board.selectedTileID[0]).piece.type == this.currPlayer && this.board.selectedTileID[1] != null){
 
                     this.prolog.getPrologRequest("makePlay((" + this.currPlayer + "," + this.board.getSelectedTile(this.board.selectedTileID[0]).coordX
                 + "," + this.board.getSelectedTile(this.board.selectedTileID[0]).coordZ + "," + this.board.getSelectedTile(this.board.selectedTileID[1]).coordX
                 + ","+ this.board.getSelectedTile(this.board.selectedTileID[1]).coordZ+"),("+ this.board.getQueen(this.currPlayer).stacks.length + "," + this.board.getQueen(this.notCurrPlayer).stacks.length + "," +
-                this.board.convertToPrologBoard()+"))", function(data){
-                    console.log(data.target.response);
+                this.board.convertToPrologBoard()+"))", (data) => {
+                    var r = data.target.response;
+                    this.reply = r.split(',');
+                    console.log(this.reply);
+
+                    console.log(this.board.selectedTileID);
+                    console.log(this.reply);
+                    if(this.reply[3] == 'false'){
+                        this.board.getClickedTile(this.board.selectedTileID[0]);
+                        this.board.getClickedTile(this.board.selectedTileID[1]);
+                        this.board.selectedTileID = [null, null];
+                        console.log(this.board.selectedTileID);                        
+                    }
+                    else{
+                        this.state = 1;
+                    }
                 });
- 
-
-
-
-
-
-
-                   /* var request = new Request(this.currPlayer,this.board.getSelectedTile(this.board.selectedTileID[0]).coordX, 
-                                this.board.getSelectedTile(this.board.selectedTileID[0]).coordZ,this.board.getSelectedTile(this.board.selectedTileID[1]).coordX,
-                                this.board.getSelectedTile(this.board.selectedTileID[1]).coordZ, this.board.getQueen(this.currPlayer).stacks.length,
-                                this.board.getQueen(this.notCurrPlayer).stacks.length, this.board.convertToPrologBoard(), this.bot, this.difficulty);
-
-                    var r = this.prolog.getPrologRequest(request);
-                    
-                    
-                    console.log(r);*/
                 }
             }
-            
+            break;
+        case 1:
+
+
+            //fazer movimento da peça
             break;
     }
     //por array de id de peças selecionadas a null no fim da joagada
